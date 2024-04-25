2d657c1bec68864691821bec7c5ecea63de711c1
Casey Robb <casey.robb@redhat.com>
2024-04-25 10:34:31 -0600

    Cleanup config

--- config.def.h
+++ config.def.h
@@ -1,21 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 2;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int baralpha       = 0xd0;
+static const unsigned int borderalpha    = OPAQUE;
+static const unsigned int borderpx       = 1;   /* border pixel of windows */
+static const unsigned int gappx          = 2;   /* gaps between windows */
+static const unsigned int snap           = 32;  /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int rmaster                 = 1;   /* 1 means master-area is initially on the right */
+static const int showbar                 = 1;   /* 0 means no bar */
 static const int showsystray             = 1;   /* 0 means no systray */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int topbar                  = 1;   /* 0 means bottom bar */
 static const char *fonts[]               = { "FiraCode Nerd Font:size=12:antialias=true" };
 static const char dmenufont[]            = "FiraCode Nerd Font:size=12:antialias=true";
-
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
 
 /* colorscheme */
 #include "nord.h"
@@ -27,7 +26,7 @@ static const char *colors[][3]      = {
 
 /* alpha */
 static const unsigned int alphas[][3]      = {
-    /*               fg      bg        border*/
+  /*               fg      bg        border*/
   [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
   [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
@@ -53,10 +52,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster        = 1;    /* number of clients in master area */
+static const int resizehints    = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 #include "layouts.c"
 static const Layout layouts[] = {
