6dcb8bf1e0f92492ba7748c89fa85a84317c2946
Casey Robb <caseyrobb@pm.me>
2025-03-23 19:47:43 -0600

    change discord icon to slack

--- config.def.h
+++ config.def.h
@@ -32,7 +32,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "󱄛", "", "󰨞", "󰈹", "󰙯", "", "󰣛", "󰓓", "󰚌" };
+static const char *tags[] = { "󱄛", "", "󰨞", "󰈹", "", "", "󰣛", "󰓓", "󰚌" };
 
 static const Rule rules[] = {
   /* xprop(1):
