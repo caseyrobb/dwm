aff8a9eea7bf70e15f5093474e1a6106d8bc6000
Casey Robb <casey.robb@redhat.com>
2025-10-19 19:47:43 -0600

    add hotkey for Nautilus

--- config.def.h
+++ config.def.h
@@ -113,7 +113,7 @@ static const Key keys[] = {
   { MODKEY,                       XK_Tab,    view,           {0} },
   { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
   { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-  { MODKEY,ControlMask            XK_f,      setlayout,      {.v = &layouts[1]} },
+  { MODKEY|ControlMask,           XK_f,      setlayout,      {.v = &layouts[1]} },
   { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
   { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
   { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
